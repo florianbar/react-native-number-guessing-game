629606a3acdba2014b4bd304354f6dbd5c0b414a
Florian Bär <florianbar01@gmail.com>
2025-03-17 10:04:34 +0200

    style button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,33 +1,46 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Platform } from "react-native";
 
-function PrimaryButton({ children, style }) {
-    return (
-        <View style={{...styles.button, ...style}}>
-            <Pressable 
-                android_ripple={{color: 'pink'}}
-                style={styles.pressable}
-                onPress={() => console.log("Hello there")}
-            >
-                <Text style={styles.buttonText}>{children}</Text>
-            </Pressable>
-        </View>
-    );
+function PrimaryButton({ children }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={() => console.log("Hello there")}
+        android_ripple={{ color: "#ab1661" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'white', 
-        borderRadius: 100,
-    },
-    pressable: {
-        width: '100%',
-        padding: 16,
-    },
-    buttonText: {
-        color: 'purple',
-        fontSize: 18,
-        textAlign: 'center',
-    },
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    ...Platform.select({
+      ios: {
+        opacity: 0.75,
+      },
+    }),
+  },
 });
 
 export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,71 +1,62 @@
-import { useState } from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { useState } from "react";
+import { TextInput, View, StyleSheet } from "react-native";
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    const [enteredText, setEnteredText] = useState("");
+  const [enteredText, setEnteredText] = useState("");
 
-    return (
-        <View style={styles.screen}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    value={enteredText}
-                    onChangeText={setEnteredText}
-                    maxLength={2}
-                    keyboardType="number-pad"
-                    autoCapitalize="none"
-                />
-            </View>
-            <View style={styles.buttonContainer}>
-                <PrimaryButton style={styles.button}>Reset</PrimaryButton>
-                <PrimaryButton style={styles.button}>Confirm</PrimaryButton>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={enteredText}
+          onChangeText={setEnteredText}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+        />
+      </View>
+      <View>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        marginTop: 100,
-        marginHorizontal: 16,
-        padding: 16,
-        borderRadius: 16,
-        elevation: 8, // android only
-        // iOS only
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-        // end - iOS only
-        backgroundColor: "#72063c",
-    },
-    inputContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginVertical: 16,
-    },
-    input: {
-        width: 60,
-        height: 55,
-        fontSize: 32,
-        fontWeight: "bold",
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        color: "#ddb52f",
-        textAlign: "center",
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 12,
-    },
-    button: {
-        flex: 1,
-    },
+  screen: {
+    marginTop: 100,
+    marginHorizontal: 16,
+    padding: 16,
+    borderRadius: 16,
+    elevation: 8, // android only
+    // iOS only
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    // end - iOS only
+    backgroundColor: "#4e0329",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 16,
+  },
+  input: {
+    width: 60,
+    height: 55,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;

18160f9a3164a2acd6946788bd7c8fcdae442279
Florian Bär <florianbar01@gmail.com>
2025-03-18 10:50:26 +0200

    remove color import

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,7 +1,5 @@
 import { Text, StyleSheet } from "react-native";
 
-import { COLORS } from "../../constants/colors";
-
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
 }
